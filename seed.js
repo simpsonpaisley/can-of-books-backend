6d0d161610b31f82bbf9ded4a2ab5c2d5cd88d36
simpsonpaisley <simpsonpaisley@gmail.com>
2023-05-11 19:32:01 +0100

    Add exampla book data to seed.

--- seed.js
+++ seed.js
@@ -7,8 +7,22 @@ const Book = require('./models/bookModel');
 
 async function seed() {
 	await Book.create({
-		name: 'Watership Down',
+		title: 'Watership Down',
 		description: 'Written by Richard Adams, a lovely story about cute bunnies.',
+		status: 'Read',
+	});
+
+	await Book.create({
+		title: 'House of Cards',
+		description:
+			'The bestselling political thriller that introduced the scheming Francis Urguhart - the most memorable politician of the last decade.',
+		status: 'Reading',
+	});
+
+	await Book.create({
+		title: 'Mrs Pepperpot Stories',
+		description:
+			'Book about an old lady who shrinks to the size of a pepperpot.',
 		status: 'Read',
 	});
 
